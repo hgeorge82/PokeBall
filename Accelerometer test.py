44b45ae351dc9eecaf5f0e312d5e6ce343949297
msadat50 <msadat50@charlottesvilleschools.org>
2023-01-30 14:04:29 -0500

    accelermeter code

--- Accelerometer test.py
+++ Accelerometer test.py
@@ -1,28 +1,26 @@
 #type: ignore
-import board
+import adafruit_mpu6050 
 import busio
-from adafruit_display_text import 
-import adafruit_mpu6050
+import board
+import time
 
-sda_pin=board.GP16
+sda_pin=board.GP14
 scl_pin=board.GP15
 i2c = busio.I2C(scl_pin, sda_pin)
-
 mpu = adafruit_mpu6050.MPU6050(i2c)
 print(mpu.acceleration )
 
 while True:
-    print ( mpu.acceleration)
-    if mpu.acceleration[0] > 9:
-        print ( mpu.acceleration[0])
-    if mpu.acceleration[0] < -9:
-        print ( mpu.acceleration[0])
-    if mpu.acceleration[1] > 9:
-        print ( mpu.acceleration[0])
-    if mpu.acceleration[1] < -9:    
-        print ( mpu.acceleration[0])
-
-
-    else: 
-    print("don't give up")
-    
+    print (mpu.acceleration)
+    if mpu.acceleration[0] > 36:
+        print (mpu.acceleration[0])
+        time.sleep(1)
+    if mpu.acceleration[0] < -36:
+        print (mpu.acceleration[0])
+        time.sleep(1)
+    if mpu.acceleration[1] > 36:
+        print (mpu.acceleration[0])
+        time.sleep(1)
+    if mpu.acceleration[1] < -36:
+        print (mpu.acceleration[0])
+        time.sleep(1)
